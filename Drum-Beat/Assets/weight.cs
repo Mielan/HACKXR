2ed9be383246ed380227913fa6c9e63c72c241c6
Xiaolan Huang <xih007@UCSD.EDU>
2019-05-05 13:36:26 -0700

    drumbeat

---
+++ Drum-Beat/Assets/weight.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weight : MonoBehaviour{
+    public float speed = 40.0f;
+    // Start is called before the first frame update
+    
+    // Update is called once per frame
+    void Start(){
+        transform.Translate(Vector3.down * speed * Time.deltaTime, Space.World);
+    }
+}
